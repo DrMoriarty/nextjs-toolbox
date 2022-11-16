00187e34eb8e088b45ed43c201f624da3e3d0c09
Vasiliy Makarov <drmoriarty.0@gmail.com>
2022-11-16 13:31:28 +0300

    new api

---
+++ netlify/functions/fakeResponse.js
@@ -0,0 +1,16 @@
+const generateRandomString = (myLength) => {
+  const chars =
+    "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+  const randomArray = Array.from(
+    { length: myLength },
+    (v, k) => chars[Math.floor(Math.random() * chars.length)]
+  );
+
+  const randomString = randomArray.join("");
+  return randomString;
+};
+
+export const fakeResponse = () => {
+  const length = Math.floor(Math.random() * 25) * 30 + 25
+  return generateRandomString(length)
+}
